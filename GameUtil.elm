419133c3a9feb9708838bcd5f78e7560521a3118
Galaczi Endre <galacziendre@gmail.com>
2016-05-14 23:07:02 +0100

    wip on grid

--- GameUtil.elm
+++ GameUtil.elm
@@ -2,46 +2,51 @@ module GameUtil where
 -- UTIL
 
 stail : List Int -> List Int
-stail xs = case List.tail xs of 
+stail xs = case List.tail xs of
   Nothing -> []
   Just tl -> tl
- 
+
 shead : List Int -> Int
-shead xs = case List.head xs of 
+shead xs = case List.head xs of
   Nothing -> 0
   Just i -> i
 
-updateone index with list = 
-   if (List.isEmpty list) then [] else 
-     case (index, (shead list)) of 
+updateone index with list =
+   if (List.isEmpty list) then [] else
+     case (index, (shead list)) of
        (0, 0) -> (with :: (stail list))
        (i, 0) -> 0 :: (updateone  (i-1) with (stail list))
        (i, sh) -> sh :: (updateone  i with (stail list))
 
 splitAt : Int -> List Int -> List ( List Int )
-splitAt k list = 
-  if List.isEmpty list then [] else  
+splitAt k list =
+  if List.isEmpty list then [] else
     [List.take k list] ++ (splitAt k (List.drop k list))
 
 transpose : List (List Int) -> List (List Int)
-transpose list = 
-  case list of 
+transpose list =
+  case list of
     [l1, l2, l3, l4] -> List.map4 (\a b c d -> [a,b,c,d]) l1 l2 l3 l4
     _ -> []
 
 
-zeroes : List Int -> List Int 
+zeroes : List Int -> List Int
 zeroes = List.filter <| (==) 0
 
-nonzeroes : List Int -> List Int 
-nonzeroes = List.filter <| (/=) 0 
+nonzeroes : List Int -> List Int
+nonzeroes = List.filter <| (/=) 0
 
 shift : List Int -> List Int
 shift board = nonzeroes board ++ zeroes board
 
-merge : List Int -> List Int 
+merge : List Int -> List Int
 merge list = case list of
-    (x::y::xs) -> 
+    (x::y::xs) ->
       if (x == y) then [x*2, 0] ++ merge xs
       else x :: merge (y::xs)
     xs -> xs
+
+range : Int -> Int -> List Int
+range x y = case x == y of
+    True -> [y]
+    False -> x :: range (x + 1) y
--- MainGame.elm
+++ MainGame.elm
@@ -1,13 +1,47 @@
 module MainGame where
 
-import Graphics.Element exposing (..)
+import Graphics.Element exposing (Element)
 import Keyboard
 import Char
-import Html exposing (text)
 import Random
 import Game exposing (myshow, gameState)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import GameUtil exposing (range)
 
+boxsize = 25
+boxpadding = 5
 
-main : Signal Element
+rectAt : Int -> Int -> Svg
+rectAt atx aty = rect [
+    x (toString atx),
+    y (toString aty),
+    width (toString boxsize),
+    height (toString boxsize)] []
+
+gridRow : Int -> Int -> List Svg
+gridRow size aty = List.map (\ x -> rectAt x aty)
+        <| List.map (\x -> boxpadding + x * (boxsize + boxpadding))
+        <| range 0 (size - 1)
+
+grid : Int -> List Svg
+grid size = range 0 (size - 1)
+        |> List.map (\x -> boxpadding + x * (boxsize + boxpadding))
+        |> List.map (gridRow 4)
+        |> List.concat
+
+gridBackground : Int -> Svg
+gridBackground size = let
+      dim = boxpadding  + (boxsize + boxpadding) * size
+    in
+       rect [
+        x "0", y "0", width (toString dim), height (toString dim),
+        Svg.Attributes.style "fill:blue;"
+       ] []
+
+view _ =
+  svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
+      <| List.concat [[gridBackground 4] , grid 4]
+
 main =
-  Signal.map myshow gameState
+  Signal.map view gameState
